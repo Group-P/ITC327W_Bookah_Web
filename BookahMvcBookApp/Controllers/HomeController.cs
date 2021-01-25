c587cb62837a4a50b0ad26a5331ba349c34efb06
Morena-Leluma <lelumaromeo@gmail.com>
2021-01-25 15:59:10 +0200

    210034408 - 25 Jan 21 Added Code to the "HomeControler" to list the items in the items in the Cart

--- BookahMvcBookApp/Controllers/HomeController.cs
+++ BookahMvcBookApp/Controllers/HomeController.cs
@@ -3,15 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using BookahMvcBookApp.Models;
 
 namespace BookahMvcBookApp.Controllers
 {
     public class HomeController : Controller
     {
+        dbBookahMvcBookAppEntities1 db = new dbBookahMvcBookAppEntities1();
+        List<Cart> li = new List<Cart>();
+
         // GET: Home
         public ActionResult Index()
         {
-            return View();
+            var query = db.tblBooks.ToList();
+            return View(query);
+        }
+
+        public ActionResult AddtoCart(int id)
+        {
+            var query = db.tblBooks.Where(x => x.BookId == id).SingleOrDefault(); 
+            return View(query);
         }
     }
 }

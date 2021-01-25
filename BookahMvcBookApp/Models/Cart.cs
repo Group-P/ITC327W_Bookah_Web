1eedcf66d70bbe7b6427a8bb5c5a6ca4d6d00f5a
Morena-Leluma <lelumaromeo@gmail.com>
2021-01-25 15:18:43 +0200

    2110034408 - 25 Jan 21 - Added a Cart class

---
+++ BookahMvcBookApp/Models/Cart.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BookahMvcBookApp.Models
+{
+    public class Cart
+    {
+        public int bookid { get; set; }
+
+        public string bookname{ get; set; }
+
+        public int quantity { get; set; }
+
+        public int price { get; set; }
+
+        public int bill { get; set; }
+    }
+}

cf33ee1fcd95ca9736bea1723e7b1c89678f8018
Lesedi-Seemane <lsdseemane@gmail.com>
2021-01-26 05:28:08 +0200

    215119142 - 26 January 2021 - Added an "Add to cart" Module

--- BookahMvcBookApp/Controllers/HomeController.cs
+++ BookahMvcBookApp/Controllers/HomeController.cs
@@ -26,10 +26,109 @@ namespace BookahMvcBookApp.Controllers
             return View(query);
         }
 
+
+
+        #region add to cart
+
         public ActionResult AddtoCart(int id)
         {
-            var query = db.tblBooks.Where(x => x.BookId == id).SingleOrDefault(); 
+            var query = db.tblBooks.Where(x => x.BookId == id).SingleOrDefault();
             return View(query);
         }
+
+        [HttpPost]
+        public ActionResult AddtoCart(int id, int qty)
+        {
+            tblBook b = db.tblBooks.Where(x => x.BookId == id).SingleOrDefault();
+            Cart c = new Cart();
+            c.bookid = id;
+            c.bookname = b.Title;
+            c.price = Convert.ToInt32(b.Unit);
+            c.quantity = Convert.ToInt32(qty);
+            c.bill = c.price * c.quantity;
+            if (TempData["cart"] == null)
+            {
+                li.Add(c);
+                TempData["cart"] = li;
+            }
+            else
+            {
+                List<Cart> li2 = TempData["cart"] as List<Cart>;
+                int flag = 0;
+                foreach (var item in li2)
+                {
+                    if (item.bookid == c.bookid)
+                    {
+                        item.quantity += c.quantity;
+                        item.bill += c.bill;
+                        flag = 1;
+                    }
+
+                }
+                if (flag == 0)
+                {
+                    li2.Add(c);
+                    //new item
+                }
+                TempData["cart"] = li2;
+
+            }
+
+            TempData.Keep();
+
+            return RedirectToAction("Index");
+        }
+
+        #endregion
+
+        #region checkout code
+
+        public ActionResult Checkout()
+        {
+            TempData.Keep();
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Checkout(string contact, string address)
+        {
+            if (ModelState.IsValid)
+            {
+                List<Cart> li2 = TempData["cart"] as List<Cart>;
+                tblInvoice iv = new tblInvoice();
+                iv.UserId = Convert.ToInt32(Session["uid"].ToString());
+                iv.InvoiceDate = System.DateTime.Now;
+                iv.Bill = (int)TempData["total"];
+                iv.Payment = "cash";
+                db.tblInvoices.Add(iv);
+                db.SaveChanges();
+                //order book
+                foreach (var item in li2)
+                {
+                    tblOrder od = new tblOrder();
+                    od.BookId = item.bookid;
+                    od.Contact = contact;
+                    od.Address = address;
+                    od.OrderDate = System.DateTime.Now;
+                    od.InvoiceId = iv.InvoiceId;
+                    od.Quantity = item.quantity;
+                    od.Unit = item.price;
+                    od.Total = item.bill;
+
+                    db.tblOrders.Add(od);
+                    db.SaveChanges();
+
+                }
+                TempData.Remove("total");
+                TempData.Remove("cart");
+                // TempData["msg"] = "Order Book Successfully!!";
+                return RedirectToAction("Index");
+            }
+
+            TempData.Keep();
+            return View();
+        }
+
+        #endregion
     }
 }
